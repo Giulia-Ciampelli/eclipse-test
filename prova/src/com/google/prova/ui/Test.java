915ba40c6ba02cc917851cd5f1b3e11bb8284d3b
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-09-29 22:55:27 +0200

    test commit

---
+++ prova/src/com/google/prova/ui/Test.java
@@ -0,0 +1,10 @@
+package com.google.prova.ui;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello"); // prova git
+		System.out.println("Ciao git"); // prova git
+	}
+
+}
